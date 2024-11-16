1b398a1593415e4ccf19471983493b6b17c134b5
Angad Gupta <127626872+Angad-Gupta-2005@users.noreply.github.com>
2024-11-16 14:38:57 +0530

    basic setup for redux

---
+++ client/src/store/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import authReduces from "./auth-slice/index";
+
+export const store = configureStore({
+  reducer: {
+    auth: "authReduces",
+  },
+});
